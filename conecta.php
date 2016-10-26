762cddfb67bcb70ebce74f8e8d9759163b0981dd
mmcerdan <mm.cerdan@gmail.com>
2016-10-26 20:15:43 -0200

    Add files via upload

---
+++ conecta.php
@@ -0,0 +1,9 @@
+<?php
+    $host="localhost";
+    $dbname="projetointegrador";
+    $port="5432";
+    $user="senac";
+    $pssword="senac123";
+    
+    $conexao=pg_connect("host=$host dbname=$dbname port=$port user=$user password=$pssword");
+?>
